37acb946cb25b10431c4bb3cc56515e3dda1bbd0
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-01-17 13:47:46 +0100

    fixing Signal providing_args parameter for Django>=4

--- rest_hooks/signals.py
+++ rest_hooks/signals.py
@@ -1,6 +1,12 @@
+import django
 from django.dispatch import Signal
 
 
-hook_event = Signal(providing_args=['action', 'instance'])
-raw_hook_event = Signal(providing_args=['event_name', 'payload', 'user'])
-hook_sent_event = Signal(providing_args=['payload', 'instance', 'hook'])
+if django.VERSION >= (4, 0,):
+    hook_event = Signal() # providing_args=['action', 'instance']
+    raw_hook_event = Signal() # providing_args=['event_name', 'payload', 'user']
+    hook_sent_event = Signal() # providing_args=['payload', 'instance', 'hook']
+else:
+    hook_event = Signal(providing_args=['action', 'instance'])
+    raw_hook_event = Signal(providing_args=['event_name', 'payload', 'user'])
+    hook_sent_event = Signal(providing_args=['payload', 'instance', 'hook'])
